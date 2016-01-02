dbbe1d6a2d4acd5550d14b0c575cfb1c433d9cda
jaska <jaskamiin@gmail.com>
2016-01-01 22:36:45 -0500

    Added some comments and tried to make things a bit cleaner.

--- util.c
+++ util.c
@@ -1,20 +1,48 @@
 #include "util.h"
 
+/*
+*	Take string and convert it to signed double.
+*  	Result can be casted to integer if wanted.
+*	
+*	TODO: Try to make explicit casting necessary for other types than double.
+*/
 double stone (char* _s0){
 	return _s0[0] == '-' ? -atof(++_s0) : atof(_s0);
 }
 
-struct token_data* prep_token_data(unsigned oLen){
+
+/*
+*	Return a token structure with a 2D square array, dimension equal to input.
+*
+*	Since for an input of length N, there will be x <= N tokens, each with
+*	length y <= N. Thus a square array of size N is created and memory 
+*	is reallocated later in tokenize() to get rid of empty space.
+*	
+*	TODO: Try to reimplement this without malloc() for speed
+*/
+struct token_data* prep_token_data(unsigned N){
 	struct token_data *t;
 	size_t i;
+
 	t = (struct token_data*) malloc (sizeof(struct token_data));
-	t->elements = oLen;
-	t->array = (char**) malloc (sizeof(char*) * oLen);
-	for (i = 0; i < oLen; ++i) t->array[i] = (char*) malloc (oLen);
+	t->array = (char**) malloc (sizeof(char*) * N);
+
+	for (i = 0; i < N; ++i) 
+		t->array[i] = (char*) malloc (N);
+
+	t->elements = N;
+
 	return t;
 }
 
-/* want to take string like "(+ 12 (square 23))" and return array of substrings like ["(", "12", "(", "square", "23", ")", ")"] */
+
+/* 	
+*	Take string and return array of its substrings by 'classification'; i.e.:
+*	parentheses and operators, numbers, and keywords (proc names, vars, etc).
+*	Whitespace is ignored.
+*	
+*	TODO: Nothing
+*/
 struct token_data *tokenize(char* exp){
 	struct token_data* t;
 	size_t i = 0, j = 0, k = 0; 
@@ -32,23 +60,21 @@ struct token_data *tokenize(char* exp){
 					t->array[j][k++] = tok; 
 					tok = exp[++i];
 				} while( (isdigit(tok)) || (tok == '.') );
-				k--; 
-				i--;
+				k--; i--;
 			} else if ( (isalpha(tok)) || (tok == '_') ) {
 				do {
 					t->array[j][k++] = tok;
 					tok = exp[++i];
 				} while ( (isalpha(tok)) || (tok == '_') || (isdigit(tok)) || (tok == '-'));
-				k--; 
-				i--;
+				k--; i--;
 			} else {
 				t->array[j][k] = tok;
 			}
 
-			t->array[j] = (char*) realloc (t->array[j], ++k + 1);	//resize the token to fit
-			t->array[j][k] = '\0'; 									//terminate string
-			j++;													//increment outer index
-			k=0;													//reset the inner index
+			t->array[j] = (char*) realloc (t->array[j], ++k + 1);	/*resize the token to 'fit'*/
+			t->array[j][k] = '\0'; 
+			j++;
+			k=0;
 		}
 	}
 
