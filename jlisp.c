731de0d6447dce55f9b0c95c07a10af778999bbb
jaska <jaskamiin@gmail.com>
2016-01-01 21:54:27 -0500

    Start moving things to util.h and util.c

--- jlisp.c
+++ jlisp.c
@@ -6,16 +6,8 @@
 #define PBUFF  	2048
 #define DEBUG	0
 
-struct token_data {
-	unsigned elements;
-	char** array;
-};
-
 char* input(FILE*, size_t);
-struct token_data* prep_token_data(unsigned);
-struct token_data* tokenize(char*);
 void repl();
-void parse(char*);
 void eval();
 
 int main(){
@@ -42,79 +34,6 @@ char* input(FILE* in, size_t size){
 	str[len++] = '\0';
 
 	return (char*)realloc(str, len);
-}
-
-struct token_data* prep_token_data(unsigned oLen){
-	struct token_data *t;
-	size_t i;
-
-	t = (struct token_data*) malloc (sizeof(struct token_data));
-//	printf("prep_token_data():\tInitializing height and width\n");
-
-	t->elements = oLen;
-
-	if (DEBUG) printf("prep_token_data():\theight = %d\n", t->elements);
-
-	t->array = (char**) malloc (sizeof(char*) * oLen);
-	
-	for (i = 0; i < oLen; ++i) t->array[i] = (char*) malloc (oLen);
-
-	if (DEBUG) printf("prep_token_data():\ttoken array prepared\n");
-
-	return t;
-}
-
-/* want to take string like "(+ 12 (square 23))" and return array of substrings like ["(", "12", "(", "square", "23", ")", ")"] */
-struct token_data *tokenize(char* exp){
-	struct token_data* t;
-	size_t i = 0, j = 0, k = 0; 
-	int size = strlen(exp);
-	char tok;
-
-	if (DEBUG) printf("tokenize():\t\texpression received:\t\"%s\"\n",exp);
-	if (DEBUG) printf("tokenize():\t\tsizeof expression:\t%d characters\n",size);
-	
-	t = prep_token_data(size);
-
-	if (DEBUG) fprintf(stdout, "\ntokenize():\t\tStarting parse...\n");
-	for (i = 0; i < size; ++i){
-//		if (DEBUG) fprintf(stdout, "\ntokenize():\t\titeration:\t%d\n", i+1);
-		tok = exp[i];
-
-		if (tok != ' '){
-			if (DEBUG) fprintf(stdout, "tokenize():\t\tcurrent token:\t%c\n", tok);
-
-			if ( (isdigit(tok)) || (tok == '-') || (tok == '.') ) {
-				if (DEBUG) fprintf(stdout, "tokenize():\t\tNumber token\n");
-				do {
-					t->array[j][k++] = tok; 
-					tok = exp[++i];
-				} while( (isdigit(tok)) || (tok == '.') );
-				k--; 
-				i--;
-			} else if ( (isalpha(tok)) || (tok == '_') ) {
-				if (DEBUG) fprintf(stdout, "tokenize():\t\tKeyword token\n");
-				do {
-					t->array[j][k++] = tok;
-					tok = exp[++i];
-				} while ( (isalpha(tok)) || (tok == '_') || (isdigit(tok)) || (tok == '-'));
-				k--; 
-				i--;
-			} else {
-				t->array[j][k] = tok;
-			}
-
-			t->array[j] = (char*) realloc (t->array[j], ++k + 1);	//resize the token to fit
-			t->array[j][k] = '\0'; 									//terminate string
-			if (DEBUG) fprintf(stdout, "tokenize():\t\tt->array[%d] = \"%s\"\n", j, t->array[j]);
-			j++;													//increment outer index
-			k=0;													//reset the inner index
-		}
-	}
-
-	t->array = (char**) realloc (t->array, j*sizeof(char*));
-	t->elements = j;
-	return t;
 }
 
 void repl(){
